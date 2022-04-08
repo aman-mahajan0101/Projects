577283f2a794f5977b80f4bf80a88eb694dfb31f
Aman Mahajan <amanmahajan987@gmail.com>
2022-04-08 21:18:36 +0530

    Final updated

--- E-Commerce/public/js/cart.js
+++ E-Commerce/public/js/cart.js
@@ -2,9 +2,11 @@ const cartContainer = document.querySelector(".cart");
 const ul = document.querySelector(".list-group");
 var items = ul.getElementsByTagName("li");
 var total = ul.lastElementChild;
+const checkout = document.querySelector(".checkout-badge");
 
 cartContainer.addEventListener("click", async (e) => {
   const tar = e.target;
+  // console.log(checkout);
   const id = e.target.getAttribute("product-id");
   const name = e.target.getAttribute("product-name");
   const price = e.target.getAttribute("product-price");
@@ -13,22 +15,37 @@ cartContainer.addEventListener("click", async (e) => {
     if (Number(tar.parentElement.nextElementSibling.innerText) === 1) {
       tar.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.style.display = "none";
 
+      var count = checkout.children[1].innerText;
+      checkout.children[1].innerHTML = `<span class="badge bg-danger">${count - 1}</span>`;
+
       for (var i = 0; i < items.length - 1; i++) {
         if (items[i].children[0].outerText.includes(name)) {
-          console.log(items[i]);
           items[i].style.display = "none";
 
           total.children[0].innerText = Number(total.children[0].innerText) - Number(price);
         }
       }
 
+      if (Number(total.children[0].innerText) === 0) {
+        total.style.display = "none";
+        total.parentElement.nextElementSibling.style.display = "none";
+        const section = document.createElement("section");
+        var x = document.createElement("img");
+        x.setAttribute("src", "https://shop.millenniumbooksource.com/static/images/cart1.png");
+        x.setAttribute("alt", "Empty Cart");
+        x.style.width = "max-content";
+        x.style.height = "max-content";
+        section.append(x);
+        section.style.justifyContent = "center";
+        section.style.display = "flex";
+        document.body.append(section);
+      }
+
       const res = await axios.delete(`/user/${id}/cart`);
     } else {
       tar.parentElement.nextElementSibling.innerText = Number(tar.parentElement.nextElementSibling.innerText) - 1;
-      console.log("Clicked");
+
       if (tar.parentElement.nextElementSibling.innerText === "1") {
-        console.log(tar);
-        // tar.classList.remove("fa-minus");
         tar.classList.add("fa-trash");
       }
 
@@ -51,7 +68,6 @@ cartContainer.addEventListener("click", async (e) => {
     tar.parentElement.previousElementSibling.innerText = Number(tar.parentElement.previousElementSibling.innerText) + 1;
 
     if (Number(tar.parentElement.previousElementSibling.innerText) >= 2) {
-      console.log(tar.parentElement.previousElementSibling.previousElementSibling.children[0]);
       tar.parentElement.previousElementSibling.previousElementSibling.children[0].classList.remove("fa-trash");
     }
     //For total amount change and quantity change
